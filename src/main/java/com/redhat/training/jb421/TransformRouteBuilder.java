314b659d8e41aea35c31fd0fcfca31217e639917
timyip3 <timyip3@gmail.com>
2021-01-13 16:42:11 +0800

    init version

---
+++ src/main/java/com/redhat/training/jb421/TransformRouteBuilder.java
@@ -0,0 +1,33 @@
+package com.redhat.training.jb421;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.XmlJsonDataFormat;
+
+public class TransformRouteBuilder extends RouteBuilder {
+
+	@SuppressWarnings("deprecation")
+	private static XmlJsonDataFormat xmlJsonDf = new XmlJsonDataFormat();
+	
+	private static final String SRCURI = "file://C://Users//01696020//codeready-workspace//TransformAndWireTap//orders//incoming?";
+	private static final String SRCPARAM = "include=order.*xml";
+	
+	@Override
+	public void configure() throws Exception {
+		// TODO Auto-generated method stub
+		from(SRCURI + SRCPARAM)
+			//.marshal().jaxb()
+			.log("XML Body: ${body}")
+			.marshal(xmlJsonDf)
+			.log("JSON Body: ${body}")
+			.filter().jsonpath("$[?(@.delivered !='true')]")
+			.wireTap("direct:jsonOrderLog")
+		.to("mock:fufillmentSystem");
+		
+		from("direct:jsonOrderLog")
+			.log("Order received: ${body}")
+		.to("mock:orderLog");
+		
+		
+	}
+
+}
